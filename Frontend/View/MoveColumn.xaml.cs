746189f0c19061ef418b55315adb846cd09d3c8b
Elyashiv <Elyashiv2468@gmail.com>
2021-06-18 03:28:58 +0300

    MoveColumn and SetColumnName XAML

---
+++ Frontend/View/MoveColumn.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Frontend.Model;
+using Frontend.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Frontend.View
+{
+    /// <summary>
+    /// Interaction logic for MoveColumn.xaml
+    /// </summary>
+    public partial class MoveColumn : Window
+    {
+        internal MoveColumnVM moveColumnvm;
+        private UserModel userModel;
+        private BoardModel boardModel;
+        private ColumnModel columnModel;
+        public MoveColumn(UserModel userModel, BoardModel boardModel, ColumnModel columnModel)
+        {
+            InitializeComponent();
+            this.userModel = userModel;
+            this.boardModel = boardModel;
+            this.columnModel = columnModel;
+            this.moveColumnvm = new MoveColumnVM(userModel, boardModel, columnModel);
+            this.DataContext = moveColumnvm;
+        }
+
+        private void MoveButton(object sender, RoutedEventArgs e)
+        {
+            if (moveColumnvm.MoveColumn())
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ Frontend/View/SetColumnName.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Frontend.Model;
+using Frontend.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Frontend.View
+{
+    /// <summary>
+    /// Interaction logic for SetColumnName.xaml
+    /// </summary>
+    public partial class SetColumnName : Window
+    {
+        private UserModel Usermodel;
+        private BoardModel boardModel;
+        private ColumnModel columnModel;
+        internal SetColumnNameVM columnSetNamevm;
+        public SetColumnName()
+        {
+            InitializeComponent();
+        }
+
+        private void SetButton(object sender, RoutedEventArgs e)
+        {
+            if (columnSetNamevm.SetColumnName() == true)
+            {
+                this.Close();
+            }
+        }
+    }
+}
